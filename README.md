64317d701326978aa0974828eb314b817ebc656a
Ronny Haase <ronnyhaase@fastmail.com>
2020-03-12 16:11:47 +0100

    Implement basic cli

---
+++ README.md
@@ -0,0 +1,31 @@
+# iex-cli
+
+IEX Cloud command line interface
+
+## Usage
+
+```sh
+$ IEX_TOKEN=pk_myiextoken iex <command> <symbol> [args]
+```
+
+Yes, your IEX token is expected to be an environment variable ;)
+
+All arguments are passed as query parameter to IEX
+
+### Example:
+
+```sh
+$ iex history AAPL --format=csv --filter=date,close --range=5d
+```
+
+### Commands:
+
+```
+history
+intraday
+previous
+price
+quote
+```
+
+Made with ♥ by Ronny Haase, © 2020
---
+++ cli.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+'use strict'
+const meow = require('meow')
+const createClient = require('iex').default
+
+const exit = (code = 0, message = '') => {
+  if (message) console.log(message)
+  process.exit(code)
+}
+
+const cli = meow(`
+  IEX Cloud command line interface
+
+  Usage
+    $ IEX_TOKEN=pk_myiextoken iex <command> <symbol> [args]
+
+    Yes, your IEX token is expected to be an environment variable ;)
+    All arguments are passed as query parameter to IEX
+
+  Example:
+    iex history AAPL --format=csv --filter=date,close --range=5d
+
+  Commands:
+    history
+    intraday
+    previous
+    price
+    quote
+
+  Made with ♥ by Ronny Haase, © 2020
+
+  https://greina.io/iex-cli
+`)
+
+const [command, symbol] = cli.input
+const params = cli.flags
+
+if (!process.env.IEX_TOKEN) exit(1, 'Your IEX token must be set as environment variable')
+if (!command) exit(1, 'Missing command')
+if (!symbol) exit(1, 'Missing symbol')
+
+const client = createClient(process.env.IEX_TOKEN)
+
+if (!client[command]) exit(1, 'Unknown command')
+
+client[command](symbol, params).then(console.log)
